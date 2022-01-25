d8562ad474543cf7e2c9c4f2ce85584ff9a42f2d
Alex Dementyev <alexander.dem345@yandex.ru>
2022-01-25 20:58:33 +0300

    draft

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,36 @@
-window.addEventListener('load', function () {});
+window.addEventListener('load', function () {
+  
+  let forms = document.querySelectorAll('form');
+  
+  for (let form of forms) {
+    form.addEventListener('submit', function(e){
+      if (!inputValidation(this)) {
+        e.preventDefault;
+      }
+    });
+  }
+
+  function inputValidation(form) {
+
+    const rules = {
+      name: /[А-Яа-яЁёa-zA-Z]{2}/,
+      email: /\w+@\w+\.\w+/,
+      tel: /(\+*[\d()-]){6}/g,
+    }
+
+    let inputs = form.querySelectorAll('.form__inputs');
+    for (let input of inputs) {
+
+      let pattern = input.dataset.pattern ? input.dataset.pattern : false;
+
+      if (input.value.match(pattern) === null){
+        return false;
+      }
+      return true;
+      
+    }
+  }
+  
+});
 
 
